a01e669d30014aad365d049fcf66e9363878fc95
VBykov <vbykov@mtsbank.ru>
2023-11-28 13:30:00 +0300

    Data

---
+++ data.sql
@@ -0,0 +1,70 @@
+/*Customers*/
+INSERT INTO customers VALUES (1, 'Company A', '8(495)123-12-12', 'a.company@gmail.com');
+INSERT INTO customers VALUES (2, 'Company B', '8(495)321-12-12', 'b.company@b.com');
+INSERT INTO customers VALUES (3, 'Company C', '8(495)111-11-11', 'c@company.com');
+INSERT INTO customers VALUES (4, 'X-Company', '8(495)222-11-14', 'x@x.com');
+INSERT INTO customers VALUES (5, 'O. Corp', '8(495)415-11-18', 'o@corp.com');
+/*Notes*/
+INSERT INTO notes VALUES (1, 1, 'Любят красные сайты');
+INSERT INTO notes VALUES (2, 2, 'Любят зелёные сайты');
+INSERT INTO notes VALUES (3, 2, 'Не любят синие сайты');
+INSERT INTO notes VALUES (4, 5, 'Не любят сайты');
+INSERT INTO notes VALUES (5, 5, 'Много платят');
+/*Orders*/
+INSERT INTO orders VALUES (1, 1, '2023-06-11', 'Создать сайт');
+INSERT INTO orders VALUES (2, 1, '2023-06-15', 'Развернуть сайт');
+INSERT INTO orders VALUES (3, 4, '2023-06-25', 'Создать чёрный сайт');
+INSERT INTO orders VALUES (4, 5, '2023-07-14', 'Создать красивый сайт');
+INSERT INTO orders VALUES (5, 5, '2023-07-30', 'Сделать сайт красивее');
+/*Tses*/
+INSERT INTO tses VALUES (1, 1, '2023-06-12', 'Создать красный дизайн сайта');
+INSERT INTO tses VALUES (2, 1, '2023-06-13', 'Создать бекэнд сайта');
+INSERT INTO tses VALUES (3, 1, '2023-06-13', 'Создать фронтэнд сайта');
+INSERT INTO tses VALUES (4, 1, '2023-06-15', 'Развернуть сайт');
+
+INSERT INTO tses VALUES (5, 4, '2023-07-14', 'Создать очень красивый дизайн сайта');
+INSERT INTO tses VALUES (6, 4, '2023-07-15', 'Создать бекэнд сайта (очень важно!)');
+INSERT INTO tses VALUES (7, 4, '2023-07-15', 'Создать фронтэнд сайта (очень важно!)');
+/*Teams*/
+INSERT INTO teams VALUES (1, 1, 'Дизайнерская команда', 5);
+INSERT INTO teams VALUES (2, 1, 'Бекэнд команда', 6);
+INSERT INTO teams VALUES (3, 1, 'Фронтэнд команда', 7);
+INSERT INTO teams VALUES (4, 0, 'Девопс команда', 4);
+/*Jobs*/
+INSERT INTO jobs VALUES (1, 'Лидер команды', 4000);
+INSERT INTO jobs VALUES (2, 'Старший разработчик', 2000);
+INSERT INTO jobs VALUES (3, 'Разработчик', 1000);
+INSERT INTO jobs VALUES (4, 'Дизайнер', 1000);
+INSERT INTO jobs VALUES (5, 'Девопс', 1500);
+/*Employees*/
+INSERT INTO employees VALUES (1, 1, 'Лидер', 'Команды', 'Бекэнда', 2);
+INSERT INTO employees VALUES (2, 2, 'Старший', 'Разработчик', 'Бекэнда', 2);
+INSERT INTO employees VALUES (3, 3, 'Просто', 'Разработчик', 'Бекэнда', 2);
+
+INSERT INTO employees VALUES (4, 1, 'Лидер', 'Команды', 'Дизайна', 1);
+INSERT INTO employees VALUES (5, 4, 'Просто', null, 'Дизайнер', 1);
+
+INSERT INTO employees VALUES (6, 1, 'Лидер', 'Команды', 'Фронтэнда', 3);
+INSERT INTO employees VALUES (7, 3, 'Просто', 'Разработчик', 'Фронтэнда', 3);
+
+INSERT INTO employees VALUES (8, 1, 'Единственный', 'В-Команде', 'Девопс', 4);
+/*Specialisations*/
+INSERT INTO specialisations VALUES (1, 'Дизайнер');
+INSERT INTO specialisations VALUES (2, 'Бекендер');
+INSERT INTO specialisations VALUES (3, 'Фронтендер');
+INSERT INTO specialisations VALUES (4, 'Девопс');
+/*employees_specialisations*/
+INSERT INTO employees_specialisations VALUES (1, 2);
+INSERT INTO employees_specialisations VALUES (1, 3);
+INSERT INTO employees_specialisations VALUES (2, 2);
+INSERT INTO employees_specialisations VALUES (3, 2);
+
+INSERT INTO employees_specialisations VALUES (4, 1);
+INSERT INTO employees_specialisations VALUES (4, 3);
+INSERT INTO employees_specialisations VALUES (5, 1);
+
+INSERT INTO employees_specialisations VALUES (6, 3);
+INSERT INTO employees_specialisations VALUES (7, 3);
+
+INSERT INTO employees_specialisations VALUES (8, 2);
+INSERT INTO employees_specialisations VALUES (8, 4);
--- tables.sql
+++ tables.sql
@@ -2,53 +2,56 @@ DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
 
 CREATE TABLE customers (
-    id INTEGER PRIMARY KEY UNIQUE,
+    id INTEGER PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     phone_number VARCHAR(255),
     email VARCHAR(255)
 );
 
 CREATE TABLE notes (
-    id INTEGER PRIMARY KEY UNIQUE,
+    id INTEGER PRIMARY KEY,
     customer_id INTEGER NOT NULL REFERENCES customers (id),
     content TEXT NOT NULL
 );
 
 CREATE TABLE orders (
-    id INTEGER PRIMARY KEY UNIQUE,
+    id INTEGER PRIMARY KEY,
     customer_id INTEGER NOT NULL REFERENCES customers (id),
     date_created DATE NOT NULL,
     description TEXT NOT NULL
 );
 
 CREATE TABLE tses (
-    id INTEGER PRIMARY KEY UNIQUE,
+    id INTEGER PRIMARY KEY,
     order_id INTEGER NOT NULL REFERENCES orders (id),
     date_created DATE NOT NULL,
     description TEXT NOT NULL
 );
 
 CREATE TABLE teams (
-    id INTEGER PRIMARY KEY UNIQUE,
-    leader_id INTEGER,
+    id INTEGER PRIMARY KEY,
     tses_done INTEGER NOT NULL DEFAULT 0,
     description TEXT NOT NULL,
     ts_id INTEGER REFERENCES tses (id)
 );
 
-CREATE TABLE employees (
-    id INTEGER PRIMARY KEY UNIQUE,
-    firstname VARCHAR(255) NOT NULL,
-    middlename VARCHAR(255) NOT NULL,
-    lastname VARCHAR(255) NOT NULL,
-    team_id INTEGER REFERENCES teams (id),
+CREATE TABLE jobs (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
     salary INTEGER NOT NULL
 );
 
-ALTER TABLE teams ADD FOREIGN KEY (leader_id) REFERENCES employees (id);
+CREATE TABLE employees (
+    id INTEGER PRIMARY KEY,
+    job_id INTEGER REFERENCES jobs (id),
+    firstname VARCHAR(255) NOT NULL,
+    middlename VARCHAR(255),
+    lastname VARCHAR(255) NOT NULL,
+    team_id INTEGER REFERENCES teams (id)
+);
 
 CREATE TABLE specialisations (
-    id INTEGER PRIMARY KEY UNIQUE,
+    id INTEGER PRIMARY KEY,
     title VARCHAR(255) NOT NULL
 );
 
